67478c435771b2df4903365e5f3be9156c8ba4c2
Aidan Liu <aidanliu2023@gmail.com>
2025-11-10 13:12:44 -0600

    Initial Commit

---
+++ Monke/MonkeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonkeApp.swift
+//  Monke
+//
+//  Created by Aidan Liu on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MonkeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MonkeTests/MonkeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MonkeTests.swift
+//  MonkeTests
+//
+//  Created by Aidan Liu on 11/10/25.
+//
+
+import Testing
+@testable import Monke
+
+struct MonkeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
